60e426df7bb88bcd9de0925a4c4453b508dd914e
xuning <xningdd@163.com>
2026-02-14 10:08:20 +0800

    feat: inflight

--- helloIm-Gateway/src/main/java/com/github/xuning888/helloim/gateway/config/GateServerProperties.java
+++ helloIm-Gateway/src/main/java/com/github/xuning888/helloim/gateway/config/GateServerProperties.java
@@ -13,6 +13,7 @@ public class GateServerProperties {
     private int coreSize;
     private int maxPoolSize;
     private int queueSize;
+    private Retry retry;
 
     public String getProtocol() {
         return protocol;
@@ -70,16 +71,58 @@ public class GateServerProperties {
         this.queueSize = queueSize;
     }
 
-    @Override
-    public String toString() {
-        return "GateServerProperties{" +
-                "protocol='" + protocol + '\'' +
-                ", port=" + port +
-                ", maxFrameSize=" + maxFrameSize +
-                ", readTimeoutSeconds=" + readTimeoutSeconds +
-                ", coreSize=" + coreSize +
-                ", maxPoolSize=" + maxPoolSize +
-                ", queueSize=" + queueSize +
-                '}';
+    public Retry getRetry() {
+        return retry;
+    }
+
+    public void setRetry(Retry retry) {
+        this.retry = retry;
+    }
+
+    public static class Retry {
+
+        // 最大重试次数
+        private int maxTimes;
+
+        // 扫描飞行消息的时间间隔
+        private long scanIntervalMs;
+
+        // 飞行消息的重试间隔时间
+        private long intervalMs;
+
+        // 调度飞行消息的工作线程数, 其实就是timingwheel的数量
+        private int workerCount;
+
+        public int getMaxTimes() {
+            return maxTimes;
+        }
+
+        public void setMaxTimes(int maxTimes) {
+            this.maxTimes = maxTimes;
+        }
+
+        public long getScanIntervalMs() {
+            return scanIntervalMs;
+        }
+
+        public void setScanIntervalMs(long scanIntervalMs) {
+            this.scanIntervalMs = scanIntervalMs;
+        }
+
+        public long getIntervalMs() {
+            return intervalMs;
+        }
+
+        public void setIntervalMs(long intervalMs) {
+            this.intervalMs = intervalMs;
+        }
+
+        public int getWorkerCount() {
+            return workerCount;
+        }
+
+        public void setWorkerCount(int workerCount) {
+            this.workerCount = workerCount;
+        }
     }
 }
